d881fdba15b81ef3effab22b6b3595ab6617959d
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-24 17:02:16 +0530

    commit

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -1,8 +1,21 @@
-import { ReactNode, useReducer } from "react";
+import { ReactNode, useEffect, useReducer } from "react";
 import { CartContext, cartReducer, initialState } from "./CartContext";
 
+const getInitialCartState = () => {
+  const cart = localStorage.getItem("cart");
+  return cart ? { cartItems: JSON.parse(cart) } : initialState;
+};
+
 export const CartProvider = ({ children }: { children: ReactNode }) => {
-  const [state, dispatch] = useReducer(cartReducer, initialState);
+  const [state, dispatch] = useReducer(
+    cartReducer,
+    initialState,
+    getInitialCartState
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cartItems));
+  }, [state.cartItems]);
 
   return (
     <CartContext.Provider value={{ state, dispatch }}>
